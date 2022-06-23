002130d87ac0021c7fb49e709d9f1cfad93d9fe4
Ekpeha Samuel Chidera <105311283+Sammydolf@users.noreply.github.com>
2022-06-23 21:16:19 +0100

    Add files via upload

---
+++ java calculator/index.js
@@ -0,0 +1,23 @@
+// adds two numbers
+function add(num1, num2) {
+    console.log(num1 + num2);
+    
+}
+
+// multiplies two numbers
+function mul(num1, num2) {
+    console.log(num1 * num2);
+    
+}
+
+// subtracts two numbers
+function sub(num1, num2) {
+    console.log(num1 - num2);
+    
+}
+
+// divides two numbers
+function div(num1, num2) {
+    console.log(num1 / num2);
+    
+}
